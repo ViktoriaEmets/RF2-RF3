cca3efd4098904addb89b0be88e1273aeff7c507
ViktoriaEmets <70963917+ViktoriaEmets@users.noreply.github.com>
2021-09-16 15:12:09 +0700

    Add files via upload

--- master/src/commands_paramerers/COMMANDS_PARAMETERS.v
+++ master/src/commands_paramerers/COMMANDS_PARAMETERS.v
@@ -1,8 +1,8 @@
-module COMMANDS_PARAMETERS      // проверка, загрузка через терминал
-                                // проверка 
-                                // ляляля
+module COMMANDS_PARAMETERS      
 #(
-     parameter WIDTH_C_P = 16
+     parameter WIDTH_C_P = 16,
+     parameter N = 15,         // от фонаря два параметра 
+     parameter M = 255         // для памяти размерность 
 )
 (
     output reg                      start,         // непрерывное формирования импульсов, в TR_MANUAL
@@ -15,8 +15,14 @@ module COMMANDS_PARAMETERS      // проверка, загрузка через
                                     tx,
                                     tp,
 
+                                    point_comm,                                  
+
                                     dir_MANUAL,    // направление вращения, в TR
                                     count_MANUAL,  // разрешение работы счетчика уже сформированных импульсов, в TR
+
+    output reg [2*WIDTH_C_P-1:0]    x_set,
+                                    i_set,
+                                    fi_set,
 
     output  reg [2*WIDTH_C_P-1:0]   F1,            // мин.частота работы станции, в TR_AUTO, TX, TP
                                     F2,            // макс.частота работы станции, в TR_AUTO, TX, TP
@@ -38,31 +44,37 @@ module COMMANDS_PARAMETERS      // проверка, загрузка через
 
     input                           clk,                    // 50 MHz
                                     rst,                    // reset
-                                    TURN_ON_RF,
-                                    syncpulse,
+                                    TURN_ON_RF,             // включить ВЧ
+                                    syncpulse,              // синхроимпульс
 
     input [31:0]                    detuning, 
                                     fi_phm,                               
 
-    input wire [15:0]               avs_s0_address,        // ширина может быть 1-64
+    input wire [20:0]               avs_s0_address,        // ширина может быть 1-64
     input wire [31:0]               avs_s0_writedata,
     output reg [31:0]               avs_s0_readdata,
     input wire                      avs_s0_write,
     input wire                      avs_s0_read   
 );
    
-    reg                                 write_addr_err;
+    reg                             write_addr_err;
     
-    reg [31:0]                          control_reg,
-                                        mode_reg;
+    reg [31:0]                      control_reg,
+                                    mode_reg,
+                                    set_reg;
+
+    reg [2*WIDTH_C_P-1:0]           x_point,
+                                    i_point,
+                                    fi_point;                                    
 
 always @(posedge clk)
     begin
       if (rst)
         begin
-          write_addr_err <= 1'b0;
-          control_reg <= 0;
-         
+          write_addr_err  <= 1'b0;
+
+          control_reg     <= 0;
+          set_reg         <= 0;
         end
       else
         begin
@@ -70,11 +82,19 @@ always @(posedge clk)
           control_reg[1] <= 1'b0; // start          2
           control_reg[2] <= 1'b0; // start_N        4
 
+          mode_reg[0] <= 1'b0;// 0-работа/ 1-настройка
+
+//--------------------перенос из SETTING-------------------------------------------------------------
+          set_reg[0] <= 1'b0; // 1-point / 0-table   
+
+          set_reg[1] <= 1'b0; // tr
+          set_reg[2] <= 1'b0; // tx
+          set_reg[3] <= 1'b0; // tp
+//----------------------------------------------------------------------------------------------------
+
           if (avs_s0_write)
             begin
               case (avs_s0_address)
-            // ASK: можно ли сделать такое перечисление 
-            //      есть  ли числа 16'hA или 16'h10?
 
             //------------------- TR_MANUAL --------------------
                 16'h0: control_reg   <= avs_s0_writedata;
@@ -97,7 +117,19 @@ always @(posedge clk)
                 16'hD: d_fi_gate2   <= avs_s0_writedata;
                 16'hE: DZ_TP        <= avs_s0_writedata;
             //--------------------- TR --------------------------
-                16'hF: auto         <= avs_s0_writedata;   
+                16'hF: auto         <= avs_s0_writedata;  
+            //--------------- настойка/работа -------------------
+                16'h10: mode_reg      <= avs_s0_writedata;
+//--------------------перенос из SETTING-------------------------------------------------------------  
+// как это реализовать?               
+                16'h11:  set_reg     <= avs_s0_writedata; // ASK; а должен ли оператор задавать этот параметр
+                                                          //      логичнее переключаться автоматом,
+                                                          //      но также нужно переключаться по команде
+                16'h12:  x_point     <= avs_s0_writedata;
+                16'h13:  i_point     <= avs_s0_writedata;
+                16'h14:  fi_point    <= avs_s0_writedata; 
+//----------------------------------------------------------------------------------------------------
+
 
                 default:
                   write_addr_err  <= 1'b1;
@@ -127,8 +159,17 @@ always @(posedge clk)
                 16'hD: avs_s0_readdata <= d_fi_gate2;
                 16'hE: avs_s0_readdata <= DZ_TP; 
             //---------------------- TR -------------------------
-                16'hF: avs_s0_readdata <= auto;   
+                16'hF: avs_s0_readdata <= auto; 
+            //--------------- настойка/работа -------------------      
+                16'h10:  avs_s0_readdata <= mode_reg;
 
+//--------------------перенос из SETTING-------------------------------------------------------------  
+// как это реализовать?               
+                16'h11:  avs_s0_readdata <= set_reg;
+                16'h12:  avs_s0_readdata <= x_point;
+                16'h13:  avs_s0_readdata <= i_point;
+                16'h14:  avs_s0_readdata <= fi_point; 
+//----------------------------------------------------------------------------------------------------                
                 default:
                     avs_s0_readdata <= 32'b0;
               endcase
@@ -141,38 +182,101 @@ always @(*)
       stop           = control_reg[0];
       start          = control_reg[1];
       start_N        = control_reg[2];
+
+//--------------------перенос из SETTING------------------------------------------------------------- 
+      point_comm     = set_reg[0];
+//----------------------------------------------------------------------------------------------------      
     end
 
 
 always @(*)
 begin
-      mode_reg[0] = 1'b0;
-      mode_reg[1] = 1'b0;
-      mode_reg[2] = 1'b0;
+  if (mode_reg[0] == 1'b0)
+    begin  
+      if (TURN_ON_RF) 
+        begin
+          tr = 1'b1;
+        end
 
-    if (TURN_ON_RF) 
-    begin
-      mode_reg[0] = 1'b1;
-    end
+      if (syncpulse)
+        begin
+          tx = 1'b1;
+        end 
 
-    if (syncpulse)
-    begin
-      mode_reg[1] = 1'b1;
+      if (fi_phm == detuning)
+        begin
+          tp = 1'b1;
+        end 
     end 
+  else        // устанавливается оператором
+   begin
+      tr = set_reg[1];
+      tx = set_reg[2];
+      tp = set_reg[3];
+    end    
+end   
 
-    if (fi_phm == detuning)
-    begin
-      mode_reg[2] = 1'b1;
-    end 
+//--------------------перенос из SETTING------------------------------------------------------------- 
+//----------------------------------- запись в память ----------------------------
+// это не точно 
+// см в шапке модуля 
+// в этом блоке  memory = x_table
 
-end  
+reg [15:0] memory   [255:0]; 
+reg [15:0] x_table  [255:0]; 
+reg [15:0] i_table  [255:0]; 
+reg [15:0] fi_table [255:0];   
+/*
+always @(posedge clk) 
+  begin
+    if (avs_s0_writedata) 
+      begin
+        memory[avs_s0_address] <= avs_s0_writedata;
+      end
+        avs_s0_readdata <= memory[avs_s0_address];       // номер шага
+  end 
 
+always @(posedge clk) 
+  begin
+    if (avs_s0_writedata)
+      begin 
+        if (tr)
+          begin
+            x_table[avs_s0_address]  <= memory[avs_s0_address];
+            avs_s0_readdata          <= x_table[avs_s0_address];
+          end
+        else if (tx)
+          begin
+            i_table[avs_s0_address]  <= memory[avs_s0_address];
+            avs_s0_readdata          <= i_table[avs_s0_address];
+          end
+        else if (tp)
+          begin
+            fi_table[avs_s0_address] <= memory[avs_s0_address];
+            avs_s0_readdata          <= fi_table[avs_s0_address]; 
+          end    
+      end
+  end         
+//-----------------------------------------------------------------------------------------------------
+*/
+
+//--------------------перенос из SETTING------------------------------------------------------------- 
 always @(*)
 begin
-      tr = mode_reg[0];
-      tx = mode_reg[1];
-      tp = mode_reg[2];
+    if (point_comm)
+        begin
+            x_set   = x_point;
+            i_set   = i_point;
+            fi_set  = fi_point;
+        end
+    else 
+        begin
+            x_set   = x_table[avs_s0_address];
+            i_set   = i_table[avs_s0_address];
+            fi_set  = fi_table[avs_s0_address];
+
+        end    
 end
-
+//-----------------------------------------------------------------------------------------------------
 
 endmodule
